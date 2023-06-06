4f7fcb9142586cb7a7eda0d58f2c529ec8006bd4
Rivenrem <rivensenik@gmail.com>
2023-06-06 17:19:00 +0300

    refactor: commands

---
+++ src/commands/index.ts
@@ -0,0 +1,17 @@
+import { HelpCommand } from "./help.command";
+import { PhotoCommand } from "./photo.command";
+import { StartCommand } from "./start.command";
+import { WeatherCommand } from "./weather.commmand";
+import { WeatherSubscribtion } from "./subscrube-weather.command";
+import { TaskCommand } from "./task.command";
+import { SuggestCommand } from "./suggestPlace.command";
+
+export default {
+  HelpCommand,
+  PhotoCommand,
+  StartCommand,
+  WeatherCommand,
+  WeatherSubscribtion,
+  TaskCommand,
+  SuggestCommand,
+};
--- src/decorators/bot.ts
+++ src/decorators/bot.ts
@@ -3,13 +3,7 @@ import { Stage } from "telegraf/typings/scenes";
 import LocalSession from "telegraf-session-local";
 
 import { Command } from "../commands/command.class";
-import { HelpCommand } from "../commands/help.command";
-import { PhotoCommand } from "../commands/photo.command";
-import { StartCommand } from "../commands/start.command";
-import { WeatherCommand } from "../commands/weather.commmand";
-import { WeatherSubscribtion } from "../commands/subscrube-weather.command";
-import { TaskCommand } from "../commands/task.command";
-import { SuggestCommand } from "../commands/suggestPlace.command";
+import commands from "../commands/index";
 
 import { addTaskScene } from "../scenes/addTask.scene";
 import { remindTaskScene } from "../scenes/remindTask.scene";
@@ -41,14 +35,14 @@ export default class Bot {
 
   init() {
     this.commands = [
-      new HelpCommand(this.bot),
-      new PhotoCommand(this.bot, "cat"),
-      new PhotoCommand(this.bot, "dog"),
-      new StartCommand(this.bot),
-      new WeatherCommand(this.bot),
-      new WeatherSubscribtion(this.bot),
-      new TaskCommand(this.bot),
-      new SuggestCommand(this.bot),
+      new commands.HelpCommand(this.bot),
+      new commands.PhotoCommand(this.bot, "cat"),
+      new commands.PhotoCommand(this.bot, "dog"),
+      new commands.StartCommand(this.bot),
+      new commands.WeatherCommand(this.bot),
+      new commands.WeatherSubscribtion(this.bot),
+      new commands.TaskCommand(this.bot),
+      new commands.SuggestCommand(this.bot),
     ];
     for (const command of this.commands) {
       command.handle();
