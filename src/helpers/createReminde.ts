270ffe9debd43405e26a4e224a7ce301180d7f13
Rivenrem <rivensenik@gmail.com>
2023-06-13 13:30:29 +0300

    refactor: change createReminder.ts schedule rule

--- src/helpers/createReminde.ts
+++ src/helpers/createReminde.ts
@@ -1,16 +1,18 @@
-import schedule from "node-schedule";
+import schedule, {Job} from "node-schedule";
 import {MyContext} from "../../types/context";
 
 const rule = new schedule.RecurrenceRule();
-rule.tz = "Europe/Minsk";
 
 export default function reminderTask(
   ctx: MyContext,
   HH: string,
   MM: string,
   task: string
-): void {
-  schedule.scheduleJob(`${MM} ${HH} * * *`, () => {
+): Job {
+  rule.tz = "Europe/Minsk";
+  rule.minute = MM;
+  rule.hour = HH;
+  return schedule.scheduleJob(rule, function () {
     if (ctx.session.chatID) {
       ctx.telegram.sendMessage(ctx.session.chatID, `Reminder: ${task}`);
     }
