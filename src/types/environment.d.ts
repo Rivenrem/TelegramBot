dd3beef7e17b431ca8c60ad603863723ceab5dc5
Rivenrem <rivensenik@gmail.com>
2023-06-14 20:10:10 +0300

    refactor: define types for process.env in encironment.d.ts

---
+++ src/types/environment.d.ts
@@ -0,0 +1,17 @@
+export {};
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            BOT_TOKEN: string;
+            DB_CONN_STRING: string;
+            OPENTRIP_API_KEY: string;
+            OPENTRIP_STATIC_URL: string;
+            PHOTOS_API_KEY: string;
+            PICS_STATIC_URL: string;
+            PORT: string;
+            WEATHER_API_KEY: string;
+            WEATHER_STATIC_URL: string;
+        }
+    }
+}
