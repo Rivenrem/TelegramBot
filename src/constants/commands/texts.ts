b4b264b05f44ec398e2b9e260d7ffbacb8fdb019
Rivenrem <rivensenik@gmail.com>
2023-06-24 20:31:25 +0300

    fix: change static url for photo folder in displayWeather.ts

--- src/constants/commands/texts.ts
+++ src/constants/commands/texts.ts
@@ -34,7 +34,7 @@ enum Weather {
     wrongLocation = "Sorry, but it's incorrect location.",
     subscribtionTime = 'Type at which hour you want to receive your weather. Send time in the 24-hours format (e.g. 08:23)',
     scheduledMessage = 'Here are your daily weather 🌤️',
-    staticSourceURL = './src/images/',
+    staticSourceURL = './src/assets/images/',
 }
 
 export const commandsText = {
